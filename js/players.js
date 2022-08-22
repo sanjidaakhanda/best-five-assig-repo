f1e9d4858a187e366fe48c16f86f2bf3da830053
Your Name <sanjida.ier@gmail.com>
2022-08-22 15:08:48 -0400

    select items added

--- js/players.js
+++ js/players.js
@@ -1,1 +1,41 @@
-console.log('log')
+// console.log('log')
+
+document.getElementById('card1-btn').addEventListener('click',function () {
+    
+    document.getElementById('select1').innerText = ' 1. David Beckham'
+
+})
+
+document.getElementById('card2-btn').addEventListener('click',function () {
+    
+    document.getElementById('select2').innerText = ' 2. Ronaldinho'
+
+})
+
+
+document.getElementById('card3-btn').addEventListener('click',function () {
+    
+    document.getElementById('select3').innerText = ' 1. David Beckham'
+
+})
+
+
+document.getElementById('card4-btn').addEventListener('click',function () {
+    
+    document.getElementById('select4').innerText = ' 4. Cristiano Ronaldo'
+
+})
+
+
+document.getElementById('card5-btn').addEventListener('click',function () {
+    
+    document.getElementById('select5').innerText = ' 5. Rolando'
+
+})
+
+
+document.getElementById('card6-btn').addEventListener('click',function () {
+    
+    document.getElementById('select6').innerText = ' 6. Mohammad Salah'
+
+})
